ca2782eb867c98826f4e1cb428a4e552d96b4cb6
kaly <kalimamadou@gmail.com>
2021-11-12 02:08:53 -0800

    C# Project: Variable and Data type manupulation

---
+++ Basic_C#_Programs/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first name, last name, age, and weight separeted by Enter.");
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            string age = Console.ReadLine();
+            string weigth = Console.ReadLine();
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+            Console.WriteLine("Age: " + age + "\nWeigth: " + weigth);
+            Console.Read();
+        }
+    }
+}
