552b9358dba057e7e67169cb25eb0a37b87a3f59
kaly <kalimamadou@gmail.com>
2021-11-16 23:23:01 -0800

    C# Loop Assignement: Part Three perform comparison in loop

--- Basic_C#_Programs/LoopIteration/Program.cs
+++ Basic_C#_Programs/LoopIteration/Program.cs
@@ -13,7 +13,7 @@ namespace LoopIteration
             // Part One  -  Array Iteration
             string[] customerName = { "kaly", "Jessica", "Salihu" };
 
-            Console.Write("Enter some text to append in the array:");
+            Console.Write("Enter some text to append in the array: ");
             string str = Console.ReadLine();
             for(int i = 0; i < customerName.Length; i++)
             {
@@ -39,8 +39,38 @@ namespace LoopIteration
             Console.WriteLine();
 
             //Part Tree - Loop with condition
-            //for (){ }
-            //do {} while ();
+            int[] arr2 = { 2, 4, 5, 6, 8, 9 };
+            Console.WriteLine("Even number from the array:");
+            for (int i = 0; i < arr2.Length; i++){
+                // print even number from the array
+                if(arr2[i] % 2 == 0)
+                {
+                    Console.Write(arr2[i] + " ");
+                }
+
+            }
+            Console.WriteLine();
+            int attemp = 1;
+            string secret = "boat";
+            char holder;
+            do {
+                Console.WriteLine("Enter any letter that can be in the secret word: ");
+                // Get only one character from user
+                holder = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                // check if secret contains the character if not increment attemp
+                if (secret.Contains(holder))
+                {
+                    Console.WriteLine($"Woow good work. the secret word is {secret}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Try again..");
+                    attemp++;
+                }
+                
+            } while (attemp <= 3);
 
             //
             // Before termination
