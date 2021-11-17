25e98cc40b1892a95cad7ce11bf65f2e7eb99907
kaly <kalimamadou@gmail.com>
2021-11-17 15:14:53 -0800

    C# Basic operation 2 passing two parameter with a default value.

---
+++ Basic_C#_Programs/MathOperation2/BasicMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperation2
+{
+    class BasicMath
+    {
+        public int addition(int num1, int num2 = 0)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ Basic_C#_Programs/MathOperation2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperation2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Basic Addition App");
+            Console.WriteLine("Please enter two number seperated by enter key: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Instantiate class
+            BasicMath calculator = new BasicMath();
+            // Display the math operation with two parameter
+            Console.WriteLine("{0} + {1} = {2}",num1, num2, calculator.addition(num1, num2));
+            // without the second number.
+            Console.WriteLine("{0} + no value = {1}", num1, calculator.addition(num1));
+
+            Console.ReadKey();
+        }
+    }
+}
