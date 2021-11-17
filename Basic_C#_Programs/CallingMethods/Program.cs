5abe8fa1a2007723329d6c65e0b87a8e0f40b865
kaly <kalimamadou@gmail.com>
2021-11-17 12:29:05 -0800

    C# Class Assigment: Build a class and its method to call later in the program.

---
+++ Basic_C#_Programs/CallingMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            arithmatic operationByTwo = new arithmatic();
+
+            Console.Write("Enter any number to have operation by two: ");
+            int val = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Adding Two to {val}: {operationByTwo.addTwo(val)}");
+            Console.WriteLine($"Divide by Two to {val}: {operationByTwo.dividebyTwo(val)}");
+            Console.WriteLine($"Multiply by Two to {val}: {operationByTwo.mutiplyByTwo(val)}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/CallingMethods/arithmatic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class arithmatic
+    {
+       
+        public  int addTwo(int x)
+        {
+            return x + 2;
+        }
+
+        public  int mutiplyByTwo(int x)
+        {
+            return x * 2;
+        }
+
+        public  int dividebyTwo(int x)
+        {
+            return x / 2;
+        }
+    }
+}
