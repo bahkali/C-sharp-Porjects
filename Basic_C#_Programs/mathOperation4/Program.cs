6e508c5ee848de0df4976f66f57d5b096c303b79
kaly <kalimamadou@gmail.com>
2021-11-19 20:22:13 -0800

    c# Assigement: basic math 4 and create the output parameter.

---
+++ Basic_C#_Programs/mathOperation4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mathOperation4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            basicMath myClass = new basicMath();
+            Console.Write("Please enter a number: ");
+            
+                int num = Convert.ToInt32(Console.ReadLine());
+                myClass.divideByTwo(num);
+            
+            
+            // End Program
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Basic_C#_Programs/mathOperation4/basicMath.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mathOperation4
+{
+    class basicMath
+    {
+        public void divideByTwo(int value)
+        {
+            Console.WriteLine($"{value} divide by 2 is {value/2}");
+        }
+        // output parameter 
+        public void addition(out int value)
+        {
+            value = 10;
+            value += 2;
+        }
+        // Overload the addition method to have two input and one output parameter
+        public void addition( int val1 , int val2, out int result)
+        {
+            result = val1 + val2;
+        }
+    }
+}
