d3d13c44439572385a7adc00a528fbc60ea766e6
kaly <kalimamadou@gmail.com>
2021-11-19 19:03:25 -0800

    Corrected the basic math assigmnet added the try catch block

--- Basic_C#_Programs/MathOperation2/Program.cs
+++ Basic_C#_Programs/MathOperation2/Program.cs
@@ -12,16 +12,22 @@ namespace MathOperation2
         {
             Console.WriteLine("Basic Addition App");
             Console.WriteLine("Please enter two number seperated by enter key: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                int num2 = Convert.ToInt32(Console.ReadLine());
 
-            // Instantiate class
-            BasicMath calculator = new BasicMath();
-            // Display the math operation with two parameter
-            Console.WriteLine("{0} + {1} = {2}",num1, num2, calculator.addition(num1, num2));
-            // without the second number.
-            Console.WriteLine("{0} + no value = {1}", num1, calculator.addition(num1));
-
+                // Instantiate class
+                BasicMath calculator = new BasicMath();
+                // Display the math operation with two parameter
+                Console.WriteLine("{0} + {1} = {2}", num1, num2, calculator.addition(num1, num2));
+                // without the second number.
+                Console.WriteLine("{0} + no value = {1}", num1, calculator.addition(num1));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadKey();
         }
     }
