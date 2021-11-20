6288b5be600c91718d0ddef58075c366772ffea0
kaly <kalimamadou@gmail.com>
2021-11-19 21:01:50 -0800

    c# Inheritance Assigment: create a person class and employe class.

---
+++ Basic_C#_Programs/CompanyA/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instatiate the employee object
+            Employee emp1 = new Employee() { FirstName = "Sample", LastName = "Student" };
+            // Print to the screen the name of the emp1 
+            emp1.SayName();
+
+            //End Program
+            Console.ReadKey();
+        }
+    }
+}
