f0c22a46a4b81e0dc522b66f6e8cb908e6b7d4d6
kaly <kalimamadou@gmail.com>
2021-11-19 19:21:54 -0800

    C-sharp Basic math: calling a void method.

---
+++ Basic_C#_Programs/MathOperation3/BasicMath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperation3
+{
+    class BasicMath
+    {
+        public void addition(int first, int second)
+        {
+            first = first + 1;
+            Console.WriteLine($"This is the second: {second}");
+        }
+    }
+}
---
+++ Basic_C#_Programs/MathOperation3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperation3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate Class
+            BasicMath myClass = new BasicMath();
+            // calling the method that take two integers as parameter 
+            myClass.addition(first : 2, second : 45);
+
+            // End of Program
+            Console.ReadKey();
+        }
+    }
+}
