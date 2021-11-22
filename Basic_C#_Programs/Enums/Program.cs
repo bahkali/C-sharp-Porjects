40f23228134154053261a0e52d5ba2bee3d5c9f2
kaly <kalimamadou@gmail.com>
2021-11-22 12:32:10 -0800

    Enum Program. Assign a value to the Enum

---
+++ Basic_C#_Programs/Enums/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the current day of week? ");
+            try
+            {
+                // Get the user input
+                string day = Console.ReadLine();
+
+                DayOfWeek newDay;
+                // Convert the input into Enum
+                bool checkDay = Enum.TryParse(day, out newDay);
+                // Output day entered
+                Console.WriteLine(day.ToString());
+                
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+            Console.ReadKey();
+        }
+        public enum DayOfWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
