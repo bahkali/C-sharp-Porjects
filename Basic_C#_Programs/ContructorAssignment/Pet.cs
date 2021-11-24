bdb3ee2a63edf0795ced33dd6457d257836d9f0a
kaly <kalimamadou@gmail.com>
2021-11-24 14:13:16 -0800

    C# TechAcademy Build chained constructor

---
+++ Basic_C#_Programs/ContructorAssignment/Pet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContructorAssignment
+{
+    class Pet
+    {
+        public Pet(): this("", 0, false)
+        {
+        }
+
+        //Chained Constructor
+        public Pet(string name, int age, bool cute)
+        {
+            this.name = name;
+            this.age = age;
+            this.cute = cute;
+        }
+        public int age { get; set; }
+        public string name { get; set; }
+        public bool cute { get; set; }
+    }
+}
---
+++ Basic_C#_Programs/ContructorAssignment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string daysOfWeek = "Monday";
+
+            Console.WriteLine($"The value of the const variable: {daysOfWeek}");
+        }
+    }
+}
