eb562e89d8f614b5d32c76b0835de3a63849297e
kaly <kalimamadou@gmail.com>
2021-11-24 18:08:30 -0800

    in Constructor assignment added a LINQ query.

--- Basic_C#_Programs/ContructorAssignment/Program.cs
+++ Basic_C#_Programs/ContructorAssignment/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ContructorAssignment
 {
@@ -9,6 +10,17 @@ namespace ContructorAssignment
         {
             const string daysOfWeek = "Monday";
 
+            string[] students = new string[] { "Lacey", "Trisha", "Gavin", "Josh", "Jon", "Landon", "Kyndreshia" };
+
+            IEnumerable<string> studentQuery = from student in students
+                                               where student.Length >= 6
+                                               select student;
+
+            foreach(string s in studentQuery)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
             Console.WriteLine($"The value of the const variable: {daysOfWeek}");
         }
     }
